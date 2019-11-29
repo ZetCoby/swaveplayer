d195a1eb5fe1850a6a45dbc0723c94579c454f67
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-11-29 15:56:12 +0200

    clean up

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,20 +10,6 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first': string;
-    /**
-    * The last name
-    */
-    'last': string;
-    /**
-    * The middle name
-    */
-    'middle': string;
-  }
   interface SwavePlayer {
     'audioTitle': string;
     'audioUrl': string;
@@ -34,38 +20,17 @@ export namespace Components {
 declare global {
 
 
-  interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {}
-  var HTMLMyComponentElement: {
-    prototype: HTMLMyComponentElement;
-    new (): HTMLMyComponentElement;
-  };
-
   interface HTMLSwavePlayerElement extends Components.SwavePlayer, HTMLStencilElement {}
   var HTMLSwavePlayerElement: {
     prototype: HTMLSwavePlayerElement;
     new (): HTMLSwavePlayerElement;
   };
   interface HTMLElementTagNameMap {
-    'my-component': HTMLMyComponentElement;
     'swave-player': HTMLSwavePlayerElement;
   }
 }
 
 declare namespace LocalJSX {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first'?: string;
-    /**
-    * The last name
-    */
-    'last'?: string;
-    /**
-    * The middle name
-    */
-    'middle'?: string;
-  }
   interface SwavePlayer {
     'audioTitle'?: string;
     'audioUrl'?: string;
@@ -73,7 +38,6 @@ declare namespace LocalJSX {
   }
 
   interface IntrinsicElements {
-    'my-component': MyComponent;
     'swave-player': SwavePlayer;
   }
 }
@@ -84,7 +48,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
   export namespace JSX {
     interface IntrinsicElements {
-      'my-component': LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
       'swave-player': LocalJSX.SwavePlayer & JSXBase.HTMLAttributes<HTMLSwavePlayerElement>;
     }
   }
---
+++ src/components/swave-player/readme.md
@@ -0,0 +1,19 @@
+# swave-player
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property     | Attribute     | Description | Type     | Default     |
+| ------------ | ------------- | ----------- | -------- | ----------- |
+| `audioTitle` | `audio-title` |             | `string` | `undefined` |
+| `audioUrl`   | `audio-url`   |             | `string` | `undefined` |
+| `captionUrl` | `caption-url` |             | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
